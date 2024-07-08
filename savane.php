4f9cc0d0b0a4f1fb0bf9c7680a7961efe8a1a44a
Assita25 <adoucancy@gmail.com>
2024-07-08 11:38:08 +0200

    ajout des images savane et ajout d'une page savane.php

---
+++ savane.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ARCADIA</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="/style/main.css">
+    <link rel="stylesheet" href="/style/main.scss">
+    
+    
+</head>
+<body>
+    <header>
+<?php
+    require "pages/header.html";
+    ?>
+     <!-- debut home section -->
+     <section class="home-jungle" id="jungle">
+     <div class="jungle-about text-center text-white">
+    <div class="jungle-about-content">
+        <p>Habitat Jungle</p>
+        <h1>Zoo Arcadia</h1>
+    </div>
+</div>
+     </section>
+<!-- end section home-->
+
+<!-- start section about jungle-->
+     <section id="about-j">
+    <div class="container">
+        <div class="heading-jungle">
+            <div class="row">
+                <div class="col-md-6">
+<div class="card">
+    <img src="images/jungle/hipopotam.jpg" alt="">
+            </div>
+        </div>
+        <div class="col-md-6">
+    <h7>presentation</h7>
+       <p>Le Zoo de La Flèche écrit actuellement l’une des pages les plus passionnantes et les plus stimulantes de son histoire. Cette aventure est avant tout une rencontre de femmes et d’hommes animés d’une passion commune et désireux d’atteindre l’excellence dans l’accomplissement des missions du parc : émerveillement des visiteurs, éducation, recherche et conservation.
+          « Une saison au zoo » témoigne aujourd’hui de ication sans faille dans la préservation de la biodiversité, menacée de toutes parts. A travers les projets qu’il porte, notre zoo fait maintenant un pari pour l’avenir : celui que notre créativité, nos valeurs de générosité, d’altruisme, d’optimisme et d’engagement total aux côtés des acteurs de la conservation, touchent le jeune public, cette génération future qui demain aura la lourde responsabilité de soigner, préserver, notre si belle planète.</p>
+        <div>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</section>
+<!-- end about-->
+
+<!-- section animaux jungle-->
+    <section id="blogs">
+    <div class="heaging-2">
+     <div class="container">
+     <div class="animaux-j"> <h2> Les Animaux Du Jungle</h2></div>
+     <div class="row">
+    <div class="col ">
+    <div class="card h-100" style="width: 18rem; ">
+      <img src="/images/jungle/python.jpg" style="height :250px;" class="card-img-top" alt="...">
+      <div class="card-body">
+        <a href="#" ><button type="button" class="blogs-btn btn-lg">Python</button></a>
+      </div>
+    </div>
+  </div>
+  <div class="col ">
+    <div class="card h-100" style="width: 18rem;">
+      <img src="/images/jungle/nasique.jpg" style="height :250px;" class="card-img-top" alt="...">
+      <div class="card-body">
+        <a href="#" ><button type="button" class="blogs-btn btn-lg">Nasique</button></a>
+      </div>
+    </div>
+  </div>
+  <div class="col ">
+    <div class="card h-100" style="width: 18rem;" >
+      <img src="/images/jungle/poco.jpg" style="height :250px;" class="card-img-top" alt="...">
+      <div class="card-body">
+        <a href="#" ><button type="button" class="blogs-btn btn-lg">Perroquet</button></a>
+      </div>
+    </div>
+  </div>
+  <div class="col ">
+    <div class="card h-100" style="width: 18rem;">
+      <img src="/images/jungle/komodo-dragon.jpg" style="height :250px;"class="card-img-top" alt="...">
+      <div class="card-body">
+        <a href="#" ><button type="button" class="blogs-btn btn-lg">Dragon-komodo</button></a>
+      </div>
+    </div>
+  </div>
+     </div>
+     <!-- 2eme tour animau-->
+
+     <div class="row" style="margin-top: 65px;">
+     <div class="col ">
+    <div class="card h-100" style="width: 18rem; ">
+      <img src="/images/jungle/mandrill.jpg" style="height :250px;" class="card-img-top" alt="...">
+      <div class="card-body">
+        <a href="#" ><button type="button" class="blogs-btn btn-lg">Mandrill</button></a>
+      </div>
+  </div>
+     </div>
+  <div class="col ">
+    <div class="card h-100" style="width: 18rem;">
+      <img src="/images/jungle/opaki.jpg" style="height :250px;" class="card-img-top" alt="...">
+      <div class="card-body">
+        <a href="#" ><button type="button" class="blogs-btn btn-lg">Opaki</button></a>
+      </div>
+    </div>
+  </div>
+  <div class="col ">
+    <div class="card h-100" style="width: 18rem;" >
+      <img src="/images/jungle/crodille.jpg" style="height :250px;" class="card-img-top" alt="...">
+      <div class="card-body">
+        <a href="#" ><button type="button" class="blogs-btn btn-lg">Crocrodille</button></a>
+      </div>
+    </div>
+  </div>
+  <div class="col ">
+    <div class="card h-100" style="width: 18rem;">
+      <img src="/images/jungle/orang-outan.jpg" style="height :250px;"class="card-img-top" alt="...">
+      <div class="card-body">
+        <a href="#" ><button type="button" class="blogs-btn btn-lg">orang-outan</button></a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- 3eme tour -->
+<div class="row" style="margin-top: 65px;">
+     <div class="col ">
+    <div class="card h-100" style="width: 18rem; ">
+      <img src="/images/jungle/tigre1.jpg" style="height :250px;" class="card-img-top" alt="...">
+      <div class="card-body">
+        <a href="#" ><button type="button" class="blogs-btn btn-lg">Tigre</button></a>
+      </div>
+  </div>
+     </div>
+  <div class="col ">
+    <div class="card h-100" style="width: 18rem;">
+      <img src="/images/jungle/hipopotam.jpg" style="height :250px;" class="card-img-top" alt="...">
+      <div class="card-body">
+        <a href="#" ><button type="button" class="blogs-btn btn-lg">hipopotam</button></a>
+      </div>
+    </div>
+  </div>
+  <div class="col ">
+    <div class="card h-100" style="width: 18rem;" >
+      <img src="/images/jungle/koalla.jpg" style="height :250px;" class="card-img-top" alt="...">
+      <div class="card-body">
+        <a href="#" ><button type="button" class="blogs-btn btn-lg">Perroquet</button></a>
+      </div>
+    </div>
+  </div>
+  <div class="col ">
+    <div class="card h-100" style="width: 18rem;">
+      <img src="/images/jungle/lemurs.jpg" style="height :250px;"class="card-img-top" alt="...">
+      <div class="card-body">
+        <a href="#" ><button type="button" class="blogs-btn btn-lg">Lemurien</button></a>
+      </div>
+    </div>
+  </div>
+     </div>
+     <!-- 4eme tour -->
+     <div class="row" style="margin-top: 65px;">
+     <div class="col ">
+    <div class="card h-100" style="width: 18rem; ">
+      <img src="/images/jungle/kankourou.jpg" style="height :250px;" class="card-img-top" alt="...">
+      <div class="card-body">
+        <a href="#" ><button type="button" class="blogs-btn btn-lg">Kangourou</button></a>
+      </div>
+  </div>
+     </div>
+  <div class="col ">
+    <div class="card h-100" style="width: 18rem;">
+      <img src="/images/jungle/singes.jpg" style="height :250px;" class="card-img-top" alt="...">
+      <div class="card-body">
+        <a href="#" ><button type="button" class="blogs-btn btn-lg">singes</button></a>
+      </div>
+    </div>
+  </div>
+  <div class="col ">
+    <div class="card h-100" style="width: 18rem;" >
+      <img src="/images/jungle/gorille.jpg" style="height :250px;" class="card-img-top" alt="...">
+      <div class="card-body">
+        <a href="#" ><button type="button" class="blogs-btn btn-lg">gorille</button></a>
+      </div>
+    </div>
+  </div>
+  <div class="col ">
+    <div class="card h-100" style="width: 18rem;">
+      <img src="/images/jungle/tigre-blanc.jpg" style="height :250px;"class="card-img-top" alt="...">
+      <div class="card-body">
+        <a href="#" ><button type="button" class="blogs-btn btn-lg">Dragon-komodo</button></a>
+      </div>
+    </div>
+  </div>
+     </div>
+ </section>
+    <!--end home section -->
+</header>
+<script src="/script.javascript/script.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+<footer>
+    <?php require "pages/footer.html";
+    ?>
+</footer>
