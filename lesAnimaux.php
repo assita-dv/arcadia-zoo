8cf1005da3c85a5ad5e77f16880993b76ae164a8
Assita25 <adoucancy@gmail.com>
2024-07-11 12:31:26 +0200

    ajout de page chouette.php

--- lesAnimaux.php
+++ lesAnimaux.php
@@ -300,7 +300,7 @@
     <div class="card h-100" style="width: 18rem;" >
       <img src="marais/chouette effraie1.jpg" style="height :250px;" class="card-img-top" alt="...">
       <div class="card-body">
-        <a href="#" ><button type="button" class="blogs-btn btn-lg">Chouette</button></a>
+        <a href="chouette.php" ><button type="button" class="blogs-btn btn-lg">Chouette</button></a>
       </div>
     </div>
   </div>
