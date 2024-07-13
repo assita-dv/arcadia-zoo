b720fe4e7fe13329f7453986ea6c2dbac2d6ac5d
Assita25 <adoucancy@gmail.com>
2024-07-13 12:18:38 +0200

    ajout de page elephant.php

--- lesAnimaux.php
+++ lesAnimaux.php
@@ -362,7 +362,7 @@
     <div class="card h-100" style="width: 18rem;">
       <img src="savane/élephant d.jpg" style="height :250px;" class="card-img-top" alt="...">
       <div class="card-body">
-        <a href="#" ><button type="button" class="blogs-btn btn-lg">Elephant</button></a>
+        <a href="elephant.php" ><button type="button" class="blogs-btn btn-lg">Elephant</button></a>
       </div>
     </div>
   </div>
