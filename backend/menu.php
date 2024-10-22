ac3564d5acb1152671959e8cae7861c449f3fcbd
Assita25 <adoucancy@gmail.com>
2024-10-22 17:13:24 +0200

    ajout du dossier css dashboard et header.php menu.php et tablemployée.php

---
+++ backend/menu.php
@@ -0,0 +1,61 @@
+<!--start section tableau menu left -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+   <!-- <link rel="stylesheet" href="css/table.css">-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+</head>
+<body>
+    
+<aside id="sidebar">
+<div class="sidebar-title">
+                <div class="sidebar-brand">
+                <span><i class="fa-solid fa-terminal" title="ARCADIA ZOO"></i></span>
+                <span class="label-title">ARCADIA ZOO</span>
+            </div>
+        </div>
+            <ul class="sidebar-list">
+                <li class="sidebar-list-item">
+                    <span><i class="fa-solid fa-table-columns" title="Tableau de bord"></i></span>
+                    <a href="/index.php"><span class="label-item ">Tableau de bord</span> </a>
+                </li>
+                <li class="sidebar-list-item">
+                    <span><i class="fa-solid fa-chart-line" title="classification"></i></span>
+                    <span class="label-item">classification</span> 
+                </li>
+                <li class="sidebar-list-item">
+                    <span><i class="fa-solid fa-users-gear" title="Les employée"></i></span>
+                   <a href="/User/showUser.php"><span class="label-item"> Les employée</span> </a>
+                </li>
+                <li class="sidebar-list-item">
+                    <span><i class="fa-solid fa-hippo" title="Animaux"></i></span>
+                    <a href="/animaux.table/animaux.php"><span class="label-item"> Les animaux</span> </a>
+                </li>
+                <li class="sidebar-list-item">
+                    <span><i class="fa-solid fa-list-check" title="Services"></i></span>
+                    <a href="/sevices-bdd.php/servicebdd.php"><span class="label-item">Services</span> </a>
+                </li>
+                <li class="sidebar-list-item">
+                    <span><i class="fa-solid fa-user-nurse" title="Rapport vétérinaire"></i></span>
+                    <span class="label-item">Rapport veterinaire</span> 
+                </li>
+                <li class="sidebar-list-item">
+                    <span><i class="fa-solid fa-calendar-days" title="Réglage"></i></span>
+                    <a href="/les-horaire-du-zoo/zoo-horaire.php"><span class="label-item">Hohaire du zoo</span> </a>
+                
+                </li>
+                <li class="sidebar-list-item">
+                    <span><i class="fa-regular fa-address-book" title="Messagerie"></i></span>
+                    <span class="label-item">Messagerie</span> 
+                </li>
+                <li class="sidebar-list-item">
+                    <span><i class="fa-solid fa-message" title="Avis de visiteurs"></i></span>
+                    <span class="label-item">Avis de visiteurs</span> 
+                </li>
+            </ul>
+        </aside>
